ded2cbd2454aad18baadb26bbe49b5250036ce18
Awais Ahmad <140373668+awais-ahmad-nasar@users.noreply.github.com>
2024-05-26 20:23:14 +0500

    Add files via upload

--- People_Counter/Counter.py
+++ People_Counter/Counter.py
@@ -1,1 +1,125 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+import torch
+import numpy as np
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+cap = cv2.VideoCapture(r"B:\Videos\people.mp4")
+
+model = YOLO(r"B:\Yolov8_Weights\yolov5n.pt")
+model.to(device)
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+mask = cv2.imread(r"B:\Yolov8_Weights\mask-1.png")
+mask = cv2.resize(mask, (1280, 720))
+
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limitsUp = [103, 161, 296, 161]
+limitsDown = [527, 489, 735, 489]
+totalCountsUp = []
+totalCountsDown = []
+
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (1280, 720))
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    imgRegion = cv2.bitwise_and(img, mask)
+
+    imgGraphics = cv2.imread(r"B:\Yolov8_Weights\graphics-1.png", cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics, (730, 260))
+    results = model(imgRegion, stream=True)
+
+    detections = np.empty((0, 5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,0), 3)
+
+            w, h = x2 - x1, y2 - y1
+            # x1, y1, w, h = box.xywh[0]
+            # cvzone.cornerRect(img, (x1, y1, w, h), l=8)
+
+            # confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            print(conf)
+
+            # class names
+            cls = int(box.cls[0])
+
+            currentClass = classNames[cls]
+
+            if currentClass == 'person' and conf > 0.3:
+                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(20, y1)), scale=0.6, offset=5,
+                #                    thickness=1)
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=8, rt=5)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 0, 255), 5)
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 5)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        print(result)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
+        # cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(20, y1)), scale=2, offset=10,
+        #                    thickness=3)
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+        # cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limitsUp[0] < cx < limitsUp[2] and limitsUp[1] - 15 < cy < limitsUp[1] + 15:
+            if totalCountsUp.count(id) == 0:
+                totalCountsUp.append(id)
+                cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
+
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[1] - 15 < cy < limitsDown[1] + 15:
+            if totalCountsDown.count(id) == 0:
+                totalCountsDown.append(id)
+                cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
+
+    # cvzone.putTextRect(img, f' Count: {len(totalCountsUp)}', (50, 50))
+    cv2.putText(img, str(len(totalCountsUp)), (929, 345), cv2.FONT_HERSHEY_PLAIN, 5, (139, 195, 75), 7)
+    cv2.putText(img, str(len(totalCountsDown)), (1191, 345), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 230), 7)
+
+    # Write the frame to the output video
+    out.write(img)
+
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageRegion", imgRegion)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release everything when job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
